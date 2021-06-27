cf08b4ab40ad37c76d39abab1191efc75d1bc958
Roberto Lozada <robertojoselozada@gmail.com>
2021-06-27 12:19:54 -0500

    Ejemplo Empresa

--- EjemploEmpresa/src/app/app.component.ts
+++ EjemploEmpresa/src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TestApp';
-  variable = "Variable desde component";
-  
+  title = 'EjemploEmpresa';
 }
---
+++ EjemploEmpresa/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmpleadosComponent,
+    EmpleadoComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
